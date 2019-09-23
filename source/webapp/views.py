c82443c4fa7241455fc330d66fdf0673784b0f5a
Alisher <shera.rashiduulu@gmail.com>
2019-09-23 14:25:45 +0600

    created index.html

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from webapp.forms import ProductForm
 from webapp.models import Product
+
+
+def index_view(request, *args, **kwargs):
+    search_query = request.GET.get('search', '')
+    if search_query:
+        products = Product.objects.filter(title__icontains=search_query)
+    else:
+        products = Product.objects.all()
+    return render(request, 'index.html', context={
+        'products': products
+    })
