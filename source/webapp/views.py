10018dac8beb58c3d62ad5dbd9c981014b423acb
Alisher <shera.rashiduulu@gmail.com>
2019-09-23 14:31:44 +0600

    created product.html

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -12,3 +12,10 @@ def index_view(request, *args, **kwargs):
     return render(request, 'index.html', context={
         'products': products
     })
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product.html', context={
+        'product': product
+    })
