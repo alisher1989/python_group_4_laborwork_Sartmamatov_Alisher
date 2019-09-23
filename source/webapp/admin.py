d493afcdc5e72d0c4b99b7f74e260fb1ca8d8fbe
Alisher <shera.rashiduulu@gmail.com>
2019-09-23 14:06:15 +0600

    created admin.py

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from webapp.models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'description', 'category', 'amount', 'price']
+    list_filter = ['name']
+    list_display_links = ['pk', 'description']
+    search_fields = ['name', 'category']
+    fields = ['name', 'description', 'category', 'amount', 'price']
+    readonly_fields = ['amount', 'price']
+
+
+admin.site.register(Product, ProductAdmin)
+
