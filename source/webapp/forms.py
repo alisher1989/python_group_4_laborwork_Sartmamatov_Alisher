4b63ad102b36a888ce0fc91cf171780ac08bcac8
Alisher <shera.rashiduulu@gmail.com>
2019-09-23 16:17:26 +0600

    Fixed labels of name and description

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -5,7 +5,7 @@ from webapp.models import PRODUCT_CATEGORY_CHOICES
 
 class ProductForm(forms.Form):
     name = forms.CharField(max_length=200, required=True, label='Name')
-    description = forms.CharField(max_length=3000, required=True, label='Text', widget=widgets.Textarea)
+    description = forms.CharField(max_length=3000, required=True, label='Description', widget=widgets.Textarea)
     category = forms.ChoiceField(choices=PRODUCT_CATEGORY_CHOICES, required=False, label='Category')
     amount = forms.IntegerField(min_value=1, required=True)
     price = forms.DecimalField(max_digits=7, decimal_places=2, required=True)
